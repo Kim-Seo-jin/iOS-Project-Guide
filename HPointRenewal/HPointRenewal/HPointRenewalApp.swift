2e78a9ef4068b37df1ed7fa51e73470e70f90094
김서진 <2127414@hyundaidept.com>
2026-03-26 11:24:22 +0900

    chore: iOS 프로젝트 초기 설정 추가

---
+++ HPointRenewal/HPointRenewal/HPointRenewalApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPointRenewalApp.swift
+//  HPointRenewal
+//
+//  Created by 김서진 on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct HPointRenewalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
